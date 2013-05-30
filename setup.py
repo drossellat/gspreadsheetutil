c33dc3e83ced2894a3c06b957291db50ca56a4e4
David Rossellat <david.rossellat@gmail.com>
2013-05-30 16:54:38 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GspreadsheetUtil',
+    version='0.1',
+    packages=['gspreadsheetutil',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
