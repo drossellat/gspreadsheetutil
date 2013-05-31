144ece0a5edadfe09a03e7d4d3a9e99555d6d1b3
David Rossellat <david.rossellat@gmail.com>
2013-05-31 00:24:24 -0700

    fixed pypi package

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='GspreadsheetUtil',
-    version='0.1',
-    packages=['gspreadsheetutil',],
+    name='gspreadwrapper',
+    version='0.1.3',
+    packages=['gspreadwrapper',],
+    author = 'david.rossellat',
+    author_email = 'david.rossellat@gmail.com',
+    url = 'http://about.me/david_rossellat',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    description='mostly having fun with google spreadsheets',
+    install_requires=[
+        "gspread >= 0.0.15",
+
+    ],
 )
